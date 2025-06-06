86e09e588882a82aca1dd11ebaadc511949c5da3
Jason Chen <js910924@gmail.com>
2025-06-06 19:01:55 +0800

    add log

--- services/fallback_price_service.go
+++ services/fallback_price_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"asset-diary/models"
 	"asset-diary/services/interfaces"
+	"log"
 )
 
 type FallbackPriceService struct {
@@ -24,6 +25,7 @@ func (s *FallbackPriceService) GetStockPrice(symbol string) (*models.TickerInfo,
 	result, err := s.primary.GetStockPrice(symbol)
 	if err != nil {
 		if s.fallback != nil && err.Error() != InvalidSymbolError {
+			log.Println("FallbackPriceService: GetStockPrice: ", err)
 			return s.fallback.GetStockPrice(symbol)
 		}
 		return nil, err
@@ -35,6 +37,7 @@ func (s *FallbackPriceService) GetCryptoPrice(symbol string) (*models.TickerInfo
 	result, err := s.primary.GetCryptoPrice(symbol)
 	if err != nil {
 		if s.fallback != nil && err.Error() != InvalidSymbolError {
+			log.Println("FallbackPriceService: GetCryptoPrice: ", err)
 			return s.fallback.GetCryptoPrice(symbol)
 		}
 		return nil, err
